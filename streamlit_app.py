1a94cb9debcfba23bb978a2cf35d5e8f0b46a257
Atul Manjhi <163382635+atulmanjhi1@users.noreply.github.com>
2024-06-23 18:03:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -180,9 +180,21 @@ def create_docx(text):
 
 def main():
     st.title("Resume Summary and Chatbot Comparison")
+    file_extensions = [
+    "aac", "adt", "adts", "accdb", "accde", "accdr", "accdt", "aif", "aifc", "aiff", 
+    "aspx", "avi", "bat", "bin", "bmp", "cab", "cda", "csv", "dif", "dll", "doc", 
+    "docm", "docx", "dot", "dotx", "eml", "eps", "exe", "flv", "gif", "htm", "html", 
+    "ini", "iso", "jar", "jpg", "jpeg", "m4a", "mdb", "mid", "midi", "mov", "mp3", 
+    "mp4", "mpeg", "mpg", "msi", "mui", "pdf", "png", "pot", "potm", "potx", "ppam", 
+    "pps", "ppsm", "ppsx", "ppt", "pptm", "pptx", "psd", "pst", "pub", "rar", "rtf", 
+    "sldm", "sldx", "swf", "sys", "tif", "tiff", "tmp", "txt", "vob", "vsd", "vsdm", 
+    "vsdx", "vss", "vssm", "vst", "vstm", "vstx", "wav", "wbk", "wks", "wma", "wmd", 
+    "wmv", "wmz", "wms", "wpd", "wp5", "xla", "xlam", "xll", "xlm", "xls", "xlsm", 
+    "xlsx", "xlt", "xltm", "xltx", "xps", "zip"
+]
 
     # Upload PDF
-    uploaded_file = st.file_uploader("Upload your PDF file", type="pdf")
+    uploaded_file = st.file_uploader("Upload your file", type= file_extensions)
 
     if uploaded_file is not None:
         # Extract text from PDF
